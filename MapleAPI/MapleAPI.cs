b9dc1eb8738e7399beaf8f5d13e60c1738fc1c51
KGW2027 <retryer_@naver.com>
2024-03-30 01:55:29 +0900

    Initial Commit

---
+++ MapleAPI/MapleAPI.cs
@@ -0,0 +1,35 @@
+﻿namespace MapleAPI;
+
+public class MapleAPI
+{
+    // Singletons
+    private static MapleAPI? instance;
+    public static MapleAPI Instance
+    {
+        get
+        {
+            return instance ??= new MapleAPI();
+        }
+    }
+
+    // Private
+    private string? apiKey;
+    
+    private MapleAPI()
+    {
+        
+    }
+
+    /// <summary>
+    ///  MapleAPI에서 사용할 API Key를 설정합니다.
+    /// </summary>
+    /// <param name="key">NEXON Open API에서 발급받은 키</param>
+    /// <returns></returns>
+    public MapleAPI SetApiKey(string key)
+    {
+        apiKey = key;
+        return this;
+    }
+    
+
+}
