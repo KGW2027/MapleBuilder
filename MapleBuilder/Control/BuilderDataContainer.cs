01f969d689a3b745c67694b427205d52e095d444
KGW2027 <retryer_@naver.com>
2024-04-06 07:52:47 +0900

    feat: 오버뷰 화면에서 추가된 장비 목록 디스플레이하는 기능 추가

--- MapleBuilder/Control/BuilderDataContainer.cs
+++ MapleBuilder/Control/BuilderDataContainer.cs
@@ -1,4 +1,6 @@
-﻿using MapleAPI.DataType;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MapleAPI.DataType;
 using MapleBuilder.View.SubFrames;
 
 namespace MapleBuilder.Control;
@@ -15,9 +17,19 @@ public class BuilderDataContainer
         }
     }
 
+    private static HashSet<string> RegisteredItemHashes = new();
+    public static ObservableCollection<MapleItem> RegisterItems = new();
+    // public static List<MapleItem> RegisterItems = new();
+
     private static void UpdateCharacterInfo()
     {
         if (charInfo == null) return;
+        foreach (MapleItem newItem in charInfo.Items)
+        {
+            if (!RegisteredItemHashes.Add(newItem.Hash)) continue;
+            RegisterItems.Add(newItem);
+        }
+        
         RenderOverview.Update(charInfo);
     }
 }
--- MapleBuilder/View/SubFrames/RenderOverview.xaml.cs
+++ MapleBuilder/View/SubFrames/RenderOverview.xaml.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using MapleAPI.DataType;
+using MapleBuilder.Control;
 
 namespace MapleBuilder.View.SubFrames;
 
 public partial class RenderOverview : UserControl
 {
     private static RenderOverview? selfInstance;
-
+    private static readonly Style? BUTTON_STYLE = (Style?)Application.Current.Resources[typeof(Button)];
+    
     public static void Update(CharacterInfo cInfo)
     {
         if (selfInstance == null) return;
@@ -35,19 +42,77 @@ public partial class RenderOverview : UserControl
         bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
         bitmapImage.StreamSource = new MemoryStream(cInfo.PlayerImage);
         bitmapImage.EndInit();
+        
         selfInstance!.Dispatcher.Invoke(() =>
         {
             selfInstance.ctCharacterImage.Source = bitmapImage;
         });
     }
-    
-    
+
+    private void OnRegisterItemsChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        Dictionary<string, MapleItem> hashToItem = new();
+        Dictionary<string, bool> hashToBool = new();
+
+        foreach (MapleItem item in BuilderDataContainer.RegisterItems)
+        {
+            hashToBool.Add(item.Hash, false);
+            hashToItem.Add(item.Hash, item);
+        }
+
+        List<UIElement> removes = new();
+        foreach (var element in ctItemListStack.Children)
+        {
+            string eHash = ((Button) element).Tag.ToString()!;
+            if (hashToBool.ContainsKey(eHash)) hashToBool[eHash] = true;
+            else removes.Add((UIElement) element);
+        }
+        
+        removes.ForEach(obj => ctItemListStack.Children.Remove(obj));
+
+        foreach (var pair in hashToItem)
+        {
+            if (hashToBool[pair.Key]) continue;
+
+            Button newButton = new Button
+            {
+                Tag = pair.Key,
+                Style = BUTTON_STYLE,
+                Content = pair.Value.DisplayName,
+                HorizontalContentAlignment = HorizontalAlignment.Left,
+                Width = 537,
+                Height = 32,
+                Padding = new Thickness(12, 0, 0, 0)
+            };
+            newButton.Click += EditItem;
+            ctItemListStack.Children.Add(newButton);
+        }
+
+    }
+
+
     public RenderOverview()
     {
         selfInstance = this;
+        BuilderDataContainer.RegisterItems.CollectionChanged += OnRegisterItemsChanged;
         
         InitializeComponent();
     }
+
+    #region Event Handler
+    private void OnSearchText(object sender, TextChangedEventArgs e)
+    {
+        
+    }
+
+    private void CreateNewItem(object sender, RoutedEventArgs e)
+    {
+        
+    }
     
-    
+    private void EditItem(object sender, RoutedEventArgs e)
+    {
+        
+    }
+    #endregion
 }
