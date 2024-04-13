c4c62852128e15145a3c779e9349ba03100b5791
KGW2027 <retryer_@naver.com>
2024-04-14 00:57:00 +0900

    feat: MapleBuilder 측에서 아이템을 관리하기 위한 기반 시스템 추가

---
+++ MapleBuilder/Control/Data/Item/ItemBase.cs
@@ -0,0 +1,20 @@
+﻿using MapleAPI.DataType;
+using MapleAPI.Enum;
+
+namespace MapleBuilder.Control.Data.Item;
+
+public class ItemBase
+{
+    public string UniqueName;
+    public string DisplayName;
+    public MapleEquipType.EquipType EquipType;
+    public MapleStatContainer StatContainer;
+
+    public ItemBase(MapleEquipType.EquipType equipType, string name)
+    {
+        UniqueName = name;
+        DisplayName = name;
+        EquipType = equipType;
+        StatContainer = new MapleStatContainer();
+    }
+}
---
+++ MapleBuilder/Control/Data/ItemDatabase.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MapleAPI.DataType.Item;
+using MapleBuilder.Control.Data.Item;
+
+namespace MapleBuilder.Control.Data;
+
+public class ItemDatabase
+{
+    private static ItemDatabase? _self;
+
+    public static ItemDatabase Instance
+    {
+        get { return _self ??= new ItemDatabase(); }
+    }
+
+    private ItemDatabase()
+    {
+        CachedItemList = new ObservableCollection<ItemBase>();
+        cachedHashes = new HashSet<string>();
+    }
+
+    public readonly ObservableCollection<ItemBase> CachedItemList;
+    private HashSet<string> cachedHashes;
+
+    public bool RegisterItem(MapleItemBase itemBase, out ItemBase? outItem, string author = "")
+    {
+        outItem = null;
+        if (!cachedHashes.Add(itemBase.Hash)) return false;
+
+        if (itemBase is MapleCommonItem commonItem)
+        {
+            CraftableItem cItem = GetCraftableItem(commonItem);
+            if (author != "") cItem.DisplayName += $" (from {author})";
+            CachedItemList.Add(cItem);
+            outItem = cItem;
+            return true;
+        }
+
+        return false;
+    }
+
+    private CraftableItem GetCraftableItem(MapleCommonItem commonItem)
+    {
+        CraftableItem item = new CraftableItem(commonItem.EquipType, commonItem.Name);
+        item.StatContainer += commonItem.Status;
+        
+        return item;
+    }
+
+}
