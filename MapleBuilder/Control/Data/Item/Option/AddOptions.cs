d2b8e968cbcc890738cee37a8be386e4ca205072
KGW2027 <retryer_@naver.com>
2024-04-14 14:52:06 +0900

    feat: 소울효과와 추가옵션에 대한 기능 일부 추가

---
+++ MapleBuilder/Control/Data/Item/Option/AddOptions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using MapleAPI.Enum;
+using MapleBuilder.Control.Data.Item.Interface;
+
+namespace MapleBuilder.Control.Data.Item.Option;
+
+public static class AddOptions
+{
+    public enum AddOptionType
+    {
+        STR, DEX, INT, LUK,
+        STR_DEX, STR_INT, STR_LUK,
+        DEX_INT, DEX_LUK, INT_LUK,
+        HP, MP, ATTACK, MAGIC, BOSS_DAMAGE, DAMAGE, ALL_STAT
+    }
+
+    private static readonly AddOptionType[] WEAPON_OPTIONS = {
+        AddOptionType.STR, AddOptionType.DEX, AddOptionType.INT, AddOptionType.LUK,
+        AddOptionType.STR_DEX, AddOptionType.STR_INT, AddOptionType.STR_LUK,
+        AddOptionType.DEX_INT, AddOptionType.DEX_LUK, AddOptionType.INT_LUK,
+        AddOptionType.HP, AddOptionType.MP, AddOptionType.ATTACK, AddOptionType.MAGIC,
+        AddOptionType.BOSS_DAMAGE, AddOptionType.DAMAGE, AddOptionType.ALL_STAT
+    };
+    
+    private static readonly AddOptionType[] ARMOR_OPTIONS = {
+        AddOptionType.STR, AddOptionType.DEX, AddOptionType.INT, AddOptionType.LUK,
+        AddOptionType.STR_DEX, AddOptionType.STR_INT, AddOptionType.STR_LUK,
+        AddOptionType.DEX_INT, AddOptionType.DEX_LUK, AddOptionType.INT_LUK,
+        AddOptionType.HP, AddOptionType.MP, AddOptionType.ATTACK, AddOptionType.MAGIC, AddOptionType.ALL_STAT
+    };
+
+    private static bool IsSupportAddOption(this ItemBase item)
+    {
+        return item is IAddOptionSupport;
+    }
+    
+    public static AddOptionType[] GetAddOptionTypes(this ItemBase item)
+    {
+        if (!item.IsSupportAddOption()) return Array.Empty<AddOptionType>();
+        return item.EquipType == MapleEquipType.EquipType.WEAPON ? WEAPON_OPTIONS : ARMOR_OPTIONS;
+    }
+
+    public static int GetAttackOption(this ItemBase item, int grade, bool isAttack = true)
+    {
+        if (!item.IsSupportAddOption()) return -1;
+        if (item.EquipType != MapleEquipType.EquipType.WEAPON) return 1 * grade;
+        int level = item.EquipData!.Level;
+        int baseAtk = isAttack
+            ? item.EquipData[MapleStatus.StatusType.ATTACK_POWER]
+            : item.EquipData[MapleStatus.StatusType.MAGIC_POWER];
+        return (int) Math.Ceiling((Math.Floor(level / 40.0) + 1) * grade * Math.Pow(1.1, Math.Max(grade - 3, 0)) * baseAtk);
+    }
+
+    public static int GetDamageOption(this ItemBase item, int grade)
+    {
+        return 1 * grade;
+    }
+
+    public static int GetBossDamageOption(this ItemBase item, int grade)
+    {
+        return 2 * grade;
+    }
+
+    public static int GetStatOption(this ItemBase item, int grade, bool isDoubleStat)
+    {
+        int baseLevel = item.EquipData!.Level;
+        if (!isDoubleStat && baseLevel == 250) baseLevel = 220;
+        return (isDoubleStat ? (int) (Math.Floor(baseLevel / 20.0) + 1) : (int) (Math.Floor(baseLevel / 40.0) + 1)) * grade;
+    }
+
+    public static int GetAllStatOption(this ItemBase item, int grade)
+    {
+        return 1 * grade;
+    }
+
+    public static int GetHpMpOption(this ItemBase item, int grade)
+    {
+        return item.EquipData!.Level * 3 * grade;
+    } 
+}
---
+++ MapleBuilder/Control/Data/Item/Option/SoulWeapon.cs
@@ -0,0 +1,86 @@
+﻿namespace MapleBuilder.Control.Data.Item.Option;
+
+public static class SoulWeapon
+{
+    public enum SoulType
+    {
+        NONE            = 0x00,
+        
+        // Tier 1
+        KALING          = 0x01,
+        KALOS           = 0x02,
+        DJUNKEL         = 0x03,
+        VERUS_HILLA     = 0x04,
+        WILL            = 0x05,
+        LUCID           = 0x06,
+        DAMIEN          = 0x07,
+        LOTUS           = 0x08, // 스우
+        MAGNUS          = 0x09,
+        CYGNUS          = 0x0A,
+        BLOODY_QUEEN    = 0x0B,
+        VELLUM          = 0x0C,
+        MURMUR          = 0x0D, // 무르무르
+        
+        // Tier 2
+        PINKBEAN        = 0x11,
+        VONBON          = 0x12,
+        PIERRE          = 0x13,
+        URUS            = 0x14,
+        
+        // Tier 3
+        AKARUM          = 0x21,
+        MOGADIN         = 0x22,
+        KHALIAIN        = 0x23,
+        JURAI           = 0x24,
+        CQ57            = 0x25,
+        FREYD           = 0x26,
+        
+        // Tier 4
+        VON_LEON        = 0x31,
+        HILLA           = 0x32,
+        
+        // Tier 5
+        PAPULATUS       = 0x41,
+        ZAKUM           = 0x42,
+        
+        EPHENIA         = 0x51, // 에피네아
+    }
+
+    public static SoulType GetSoulType(string name)
+    {
+        return name switch
+        {
+            "카링" => SoulType.KALING,
+            "칼로스" => SoulType.KALOS,
+            "듄켈" => SoulType.DJUNKEL,
+            "진 힐라" => SoulType.VERUS_HILLA,
+            "윌" => SoulType.WILL,
+            "루시드" => SoulType.LUCID,
+            "데미안" => SoulType.DAMIEN,
+            "스우" => SoulType.LOTUS,
+            "전투병기 스우" => SoulType.LOTUS,
+            "매그너스" => SoulType.MAGNUS,
+            "시그너스" => SoulType.CYGNUS,
+            "블러디퀸" => SoulType.BLOODY_QUEEN,
+            "벨룸" => SoulType.VELLUM,
+            "무르무르" => SoulType.MURMUR,
+            "핑크빈" => SoulType.PINKBEAN,
+            "반반" => SoulType.VONBON,
+            "피에르" => SoulType.PIERRE,
+            "우르스" => SoulType.URUS,
+            "아카이럼" => SoulType.AKARUM,
+            "모카딘" => SoulType.MOGADIN,
+            "카리아인" => SoulType.KHALIAIN,
+            "줄라이" => SoulType.JURAI,
+            "CQ57" => SoulType.CQ57,
+            "플레드" => SoulType.FREYD,
+            "반레온" => SoulType.VON_LEON,
+            "힐라" => SoulType.HILLA,
+            "파풀라투스" => SoulType.PAPULATUS,
+            "자쿰" => SoulType.ZAKUM,
+            "에피네아" => SoulType.EPHENIA,
+            _ => SoulType.NONE
+        };
+    }
+    
+}
