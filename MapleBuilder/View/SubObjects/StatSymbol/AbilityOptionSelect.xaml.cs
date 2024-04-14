5ed67168ff1a8247494f6522ac4b942418d4dee9
KGW2027 <retryer_@naver.com>
2024-04-15 00:27:56 +0900

    feat: 어빌리티 옵션 변경에 대한 부분을 서브패널로 분리

---
+++ MapleBuilder/View/SubObjects/StatSymbol/AbilityOptionSelect.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using MapleAPI.Enum;
+
+namespace MapleBuilder.View.SubObjects.StatSymbol;
+
+public partial class AbilityOptionSelect : UserControl
+{
+    private static readonly Dictionary<string, MapleStatus.StatusType> CACHED_OPTION = new();
+    
+    private MaplePotentialGrade.GradeType optionGrade;
+    private bool IsUp => IsUpValue.IsChecked!.Value;
+    private MapleStatus.StatusType selectedOption;
+    private string ComboBoxStr => MapleAbility.GetAbilityString(selectedOption).Replace("%d", ((int) ValueSlider.Value).ToString());
+
+    public AbilityOptionSelect()
+    {
+        InitializeComponent();
+        optionGrade = MaplePotentialGrade.GradeType.RARE;
+        selectedOption = MapleStatus.StatusType.OTHER;
+        OnChangedGrade();
+    }
+
+    public void OnGradeChanged(MaplePotentialGrade.GradeType newGrade)
+    {
+        optionGrade = newGrade;
+        if (!IsTop)
+            optionGrade = (MaplePotentialGrade.GradeType) Math.Max((int) optionGrade - 2, 1);
+        OnChangedGrade();
+    }
+    
+    #region XAML Property
+
+    public static readonly DependencyProperty IsTopProperty =
+        DependencyProperty.Register("IsTop", typeof(bool), typeof(AbilityOptionSelect), new PropertyMetadata(false,
+            (o, args) =>
+            {
+                var control = (AbilityOptionSelect) o;
+                bool isTop = (bool) args.NewValue;
+                control.IsUpValue.Visibility = isTop ? Visibility.Collapsed : Visibility.Visible;
+            }));
+
+    public bool IsTop
+    {
+        get => (bool) GetValue(IsTopProperty);
+        set => SetValue(IsTopProperty, value);
+    }
+    
+    #endregion
+
+    private void OnAbilitySelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems.Count == 0 || !IsInitialized) return;
+        
+        string newSelect = e.AddedItems[0]!.ToString()!;
+        selectedOption = CACHED_OPTION.GetValueOrDefault(newSelect, MapleStatus.StatusType.OTHER);
+        OnChangedValue();
+    }
+
+    private void OnSliderChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        DisplayAbility.Text = ComboBoxStr;
+    }
+
+    private void OnChangedValue()
+    {
+        if (selectedOption == MapleStatus.StatusType.OTHER)
+        {
+            ValueSlider.Minimum = 1;
+            ValueSlider.Maximum = 1;
+            ValueSlider.SmallChange = 1;
+            ValueSlider.Value = 1;
+            return;
+        }
+        
+        MaplePotentialGrade.GradeType targetGrade = optionGrade;
+        if (IsUp) targetGrade += 1;
+
+        int[] options = MapleAbility.GetMinMax(selectedOption, targetGrade);
+        
+        if (options[0] == 0 && options[1] == 0)
+        {
+            selectedOption = MapleStatus.StatusType.OTHER;
+            OnChangedGrade();
+            return;
+        }
+        
+        ValueSlider.Minimum = options[0];
+        ValueSlider.Maximum = options[1];
+        ValueSlider.SmallChange = options.Length == 3 ? options[2] : 1;
+        ValueSlider.Value = options[0];
+        
+    }
+
+    private void OnChangedGrade()
+    {
+        MaplePotentialGrade.GradeType targetGrade = optionGrade;
+        if (IsUp) targetGrade += 1;
+        
+        DisplayAbility.Items.Clear();
+        foreach (MapleStatus.StatusType status in Enum.GetValues<MapleStatus.StatusType>())
+        {
+            string display = MapleAbility.GetAbilityString(status);
+            if (display.Equals("잡옵")) continue;
+            int[] options = MapleAbility.GetMinMax(status, targetGrade);
+            if (options[0] == 0 && options[1] == 0) continue;
+            
+            display = display.Replace("%d", "[ ]");
+            CACHED_OPTION.TryAdd(display, status);
+            DisplayAbility.Items.Add(display);
+        }
+        DisplayAbility.Items.Add("잡옵");
+        
+        OnChangedValue();
+    }
+
+    private void OnCheckboxChecked(object sender, RoutedEventArgs e)
+    {
+        OnChangedGrade();
+    }
+
+    private void OnCheckboxUnchecked(object sender, RoutedEventArgs e)
+    {
+        OnChangedGrade();
+    }
+
+    private void OnDropdownClosed(object? sender, EventArgs e)
+    {
+        DisplayAbility.Text = ComboBoxStr;
+    }
+}
---
+++ MapleBuilder/View/SubObjects/StatSymbol/AbilityPanel.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using MapleAPI.Enum;
+
+namespace MapleBuilder.View.SubObjects.StatSymbol;
+
+public partial class AbilityPanel : UserControl
+{
+    private MaplePotentialGrade.GradeType abilityGrade;
+    private readonly List<AbilityOptionSelect> optionSelects;
+    
+    public AbilityPanel()
+    {
+        InitializeComponent();
+
+        abilityGrade = MaplePotentialGrade.GradeType.LEGENDARY;
+        
+        optionSelects = new List<AbilityOptionSelect>();
+        foreach (var element in MainGrid.GetChildren<AbilityOptionSelect>())
+        {
+            optionSelects.Add(element);
+            element.OnGradeChanged(abilityGrade);
+        }
+
+        AbilityGradeSelector.Items.Clear();
+        AbilityGradeSelector.Items.Add(MaplePotentialGrade.GetPotentialGradeString(MaplePotentialGrade.GradeType.EPIC));
+        AbilityGradeSelector.Items.Add(MaplePotentialGrade.GetPotentialGradeString(MaplePotentialGrade.GradeType.UNIQUE));
+        AbilityGradeSelector.Items.Add(MaplePotentialGrade.GetPotentialGradeString(MaplePotentialGrade.GradeType.LEGENDARY));
+    }
+
+    private void OnAbilityGradeChanged(object sender, SelectionChangedEventArgs e)
+    {
+        abilityGrade = MaplePotentialGrade.GetPotentialGrade(AbilityGradeSelector.Text);
+        foreach (AbilityOptionSelect element in optionSelects)
+            element.OnGradeChanged(abilityGrade);
+        
+    }
+}
